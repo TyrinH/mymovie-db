a1b22f22603d4cb860be7f021b5032cc5aad670c
Tyrin Holloway <tychnk@gmail.com>
2021-10-25 19:42:23 -0500

    used mongoose to link server to database

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//Mongoose connection to DB
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/myMovie');
+
+
+
 app.get('/', (req, res) => {
     res.send('HELLO WORLD!')
 })
