877d55c43d9eb493c7bbe978fe4527e71a4c78a1
Tyrin Holloway <tychnk@gmail.com>
2021-11-03 09:03:13 -0500

    added error handling route so express does not have unhandled basic errors

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 3000
 const path = require('path')
 const ejsMate = require('ejs-mate')
+const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
 
 //Mongoose connection to DB
@@ -26,43 +28,52 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended : true}))
 app.use(methodOverride('_method'))
 
-app.get('/movies', async (req, res) => {
+app.get('/movies', catchAsync(async (req, res) => {
   const movies = await Movie.find({})
   res.render('movies/index', { movies })
-})
+}))
 
 app.get('/movies/new', (req, res) => {
   res.render('movies/new')
 })
 
-app.post('/movies', async (req, res) => {
+app.post('/movies', catchAsync(async (req, res) => {
   const newMovie = new Movie(req.body)
   await newMovie.save();
   res.redirect(`movies/${newMovie._id}`)
-})
+}))
 
-app.get('/movies/:id', async (req, res) => {
+app.get('/movies/:id', catchAsync(async (req, res) => {
   const movie = await Movie.findById(req.params.id)
   res.render('movies/show', { movie })
-})
+}))
 
-app.get('/movies/:id/edit', async (req, res) => {
+app.get('/movies/:id/edit', catchAsync(async (req, res) => {
   const movie = await Movie.findById(req.params.id)
   res.render('movies/edit', { movie })
-})
+}))
 
-app.put('/movies/:id', async (req, res) => {
+app.put('/movies/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   const movie = await Movie.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
   res.redirect(`/movies/${movie._id}`)
 
-})
+}))
 
-app.delete('/movies/:id', async (req, res) => {
+app.delete('/movies/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   await Movie.findByIdAndDelete(id)
   res.redirect('/movies')
 
+}))
+
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page not found', 404))
+})
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = 'SOmething went wrong' } = err;
+  res.status(statusCode).render('error');
 })
 
 
