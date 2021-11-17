47717ca02fb60e27bf0ec360e11e45933fcea6c7
Tyrin Holloway <tychnk@gmail.com>
2021-11-17 09:15:58 -0600

    Added passport and user model to index file as well as adding passport session

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const session = require('express-session')
 const flash = require('connect-flash')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/user')
 
 const movies = require('./routes/movies')
 const reviews = require('./routes/reviews')
@@ -42,6 +45,10 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig))
 app.use(flash())
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
 
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
