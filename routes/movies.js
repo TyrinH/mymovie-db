31767d874358e94b2253667409a3fded941948b0
Tyrin Holloway <tychnk@gmail.com>
2021-11-17 10:24:07 -0600

    Added basic user authentication in and made it so user is redirected away from pages if not logged in

--- routes/movies.js
+++ routes/movies.js
@@ -4,69 +4,70 @@ const catchAsync = require('../utils/catchAsync')
 const ExpressError = require('../utils/ExpressError')
 const Movie = require('../models/movie');
 const { movieSchema } = require('../schemas.js')
+const isLoggedIn  = require('../middleware')
 
 
 const validateMovie = (req, res, next) => {
-    const { error } = movieSchema.validate(req.body);
-    if (error) {
-      const msg = error.details.map(el => el.message).join(',')
-      throw new ExpressError(msg, 400)
-    } else {
-      next();
-    }
+  const { error } = movieSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg, 400)
+  } else {
+    next();
   }
+}
 
 
 //Routes for movies
 
 router.get('/', catchAsync(async (req, res) => {
-    const movies = await Movie.find({})
-    res.render('movies/index', { movies })
-  }))
-  
-  router.get('/new', (req, res) => {
-    res.render('movies/new')
-  })
-  
-  router.post('/', validateMovie, catchAsync(async (req, res, next) => {
-    const newMovie = new Movie(req.body)
-    await newMovie.save();
-    req.flash('success', 'Successfully made a new movie!')
-    res.redirect(`movies/${newMovie._id}`)
-  }))
-  
-  router.get('/:id', catchAsync(async (req, res) => {
-    const movie = await Movie.findById(req.params.id).populate('reviews');
-    if(!movie){
-      req.flash('error', 'Movie not found.')
-      return res.redirect('/movies')
-    }
-    res.render('movies/show', { movie })
-  }))
-  
-  router.get('/:id/edit', catchAsync(async (req, res) => {
-    const movie = await Movie.findById(req.params.id)
-    if(!movie){
-      req.flash('error', 'Movie not found.')
-      return res.redirect('/movies')
-    }
-    res.render('movies/edit', { movie })
-  }))
-  
-  router.put('/:id', validateMovie, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const movie = await Movie.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
-    req.flash('success', 'Successfully updated movie!' )
-    res.redirect(`/movies/${movie._id}`)
-  
-  }))
-  
-  router.delete('/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Movie.findByIdAndDelete(id)
-    req.flash('success', 'successfully deleted a movie!')
-    res.redirect('/movies')
-  
-  }))
+  const movies = await Movie.find({})
+  res.render('movies/index', { movies })
+}))
 
-  module.exports = router;
+router.get('/new', isLoggedIn, (req, res) => {
+  res.render('movies/new')
+})
+
+router.post('/', isLoggedIn, validateMovie, catchAsync(async (req, res, next) => {
+  const newMovie = new Movie(req.body)
+  await newMovie.save();
+  req.flash('success', 'Successfully made a new movie!')
+  res.redirect(`movies/${newMovie._id}`)
+}))
+
+router.get('/:id', catchAsync(async (req, res) => {
+  const movie = await Movie.findById(req.params.id).populate('reviews');
+  if (!movie) {
+    req.flash('error', 'Movie not found.')
+    return res.redirect('/movies')
+  }
+  res.render('movies/show', { movie })
+}))
+
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
+  const movie = await Movie.findById(req.params.id)
+  if (!movie) {
+    req.flash('error', 'Movie not found.')
+    return res.redirect('/movies')
+  }
+  res.render('movies/edit', { movie })
+}))
+
+router.put('/:id', isLoggedIn, validateMovie, catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const movie = await Movie.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+  req.flash('success', 'Successfully updated movie!')
+  res.redirect(`/movies/${movie._id}`)
+
+}))
+
+router.delete('/:id', catchAsync(async (req, res) => {
+  const { id } = req.params;
+  await Movie.findByIdAndDelete(id)
+  req.flash('success', 'successfully deleted a movie!')
+  res.redirect('/movies')
+
+}))
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,34 @@
 const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user')
+const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
+const passport = require('passport');
 
 router.get('/register', (req, res) => {
     res.render('users/register')
+})
+router.post('/register', catchAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to myMovie DB!')
+        res.redirect('/movies')
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+
+}))
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'welcome back!');
+    res.redirect('/movies')
 })
 
 module.exports = router;
