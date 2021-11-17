64f2a8197fe028d0c765a3c0cc2e60cc14dfde9e
Tyrin Holloway <tychnk@gmail.com>
2021-11-17 15:38:19 -0600

    functionality for logging in user after new user registers added and created logic to hide navbar login button if user is logged in and only now displays logout button in navbar

--- routes/users.js
+++ routes/users.js
@@ -8,11 +8,14 @@ const passport = require('passport');
 router.get('/register', (req, res) => {
     res.render('users/register')
 })
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if(err) return next(err);
+        })
         req.flash('success', 'Welcome to myMovie DB!')
         res.redirect('/movies')
     } catch (e) {
