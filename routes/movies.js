7ffa7cf3554a0a093a99a79700f937f1cc716820
Tyrin Holloway <tychnk@gmail.com>
2021-11-18 08:02:14 -0600

    made middleware to validate is a user is an author before allowing them to edit a movie

--- routes/movies.js
+++ routes/movies.js
@@ -4,7 +4,7 @@ const catchAsync = require('../utils/catchAsync')
 const ExpressError = require('../utils/ExpressError')
 const Movie = require('../models/movie');
 const { movieSchema } = require('../schemas.js')
-const isLoggedIn  = require('../middleware')
+const isLoggedIn = require('../middleware')
 
 
 const validateMovie = (req, res, next) => {
@@ -15,6 +15,16 @@ const validateMovie = (req, res, next) => {
   } else {
     next();
   }
+}
+
+const isAuthor = async (req, res, next) => {
+  const { id } = req.params
+  const movie = await Movie.findById(id);
+  if (!movie.author.equals(req.user._id)) {
+    req.flash('error', 'You do not have permission to do this')
+    return res.redirect(`/movies/${id}`);
+  }
+  next();
 }
 
 
@@ -46,7 +56,7 @@ router.get('/:id', catchAsync(async (req, res) => {
   res.render('movies/show', { movie })
 }))
 
-router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, isAuthor,  catchAsync(async (req, res) => {
   const movie = await Movie.findById(req.params.id)
   if (!movie) {
     req.flash('error', 'Movie not found.')
@@ -55,20 +65,16 @@ router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
   res.render('movies/edit', { movie })
 }))
 
-router.put('/:id', isLoggedIn, validateMovie, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, isAuthor, validateMovie, catchAsync(async (req, res) => {
   const { id } = req.params;
-  const movie = await Movie.findById(id);
-  if(!movie.author.equal(req.user._id)) {
-    res.flash('error', 'You do not have permission to do this')
-    return res.redirect(`/movies/${id}`);
-  }
+
   const movieChange = await Movie.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
   req.flash('success', 'Successfully updated movie!')
   res.redirect(`/movies/${movieChange._id}`)
 
 }))
 
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isAuthor, catchAsync(async (req, res) => {
   const { id } = req.params;
   await Movie.findByIdAndDelete(id)
   req.flash('success', 'successfully deleted a movie!')
