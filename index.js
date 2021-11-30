a525ff35c2215457c5c500ae52053ee9c7f13cb7
Tyrin Holloway <tychnk@gmail.com>
2021-11-29 19:08:39 -0600

    added filtering for query string to prevent certain characters from entering the query for security

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
-const User = require('./models/user')
+const User = require('./model(s/user')
+const mongoSanitize = require('express-mongo-sanitize')
 
 const moviesRoutes = require('./routes/movies')
 const reviewsRoutes = require('./routes/reviews')
@@ -34,6 +35,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended : true}))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize())
 
 const sessionConfig = {
   secret: 'needasecret',
